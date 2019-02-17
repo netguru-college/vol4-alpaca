57c5a37e76fa799e5d497649abda54e5459192c6
Szymon Kuleczka <37552821+c00lerxo@users.noreply.github.com>
2019-02-17 19:10:06 +0100

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-=======
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -14,7 +6,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
->>>>>>> master
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -44,8 +35,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-<<<<<<< HEAD
-=======
   config.include FactoryBot::Syntax::Methods
 
   Shoulda::Matchers.configure do |config|
@@ -54,7 +43,6 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
->>>>>>> master
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
