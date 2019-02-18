ffd87076785f779a65881a77b50a63b189888e8a
katguz3485 <katarzyna.guzik01@gmail.com>
2019-02-18 15:29:16 +0100

    Fix user controller

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+
+
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
+
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+
+  resources :users
 
   get '/auth/failure' => 'omniauth_callbacks#failure'
   root 'home#welcome'
