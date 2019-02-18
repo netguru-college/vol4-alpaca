d39bc01140e0ec318815d7e6529b60d5c25d71ef
voycik <voycik@gmail.com>
2019-02-19 00:53:07 +0100

    Buying Alpacas is possiblegit add -A! :D

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,17 @@ Rails.application.routes.draw do
   root 'home#welcome'
   get 'dashboard', to: "home#dashboard", as: :dashboard
   get 'market_place', to: "alpacas#market_place", as: :market_place
+  #put 'change_owner', to: "ownerships#change_owner", as: :change_
 
 
   resources :alpacas
   resources :users
+  #resources :ownerships
 
   resources :alpacas do
     put :sell, on: :member
+  end
+  resources :ownerships do
+    put :change_owner, on: :member
   end
 end
