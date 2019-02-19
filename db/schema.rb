1627909875a24cedda8b209f7b2df3af6a34592f
c00lerxo <szymonkuleczka98@gmail.com>
2019-02-19 16:37:38 +0100

    Change URL data types from String to Text in Users and Alpacas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_150250) do
+ActiveRecord::Schema.define(version: 2019_02_19_152540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_150250) do
   create_table "alpacas", force: :cascade do |t|
     t.string "name", null: false
     t.string "quote", null: false
-    t.string "picture_url", null: false
+    t.text "picture_url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "gender", null: false
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_150250) do
     t.string "name"
     t.string "provider"
     t.string "uid"
-    t.string "avatar_url", default: "https://st3.depositphotos.com/6811030/13096/v/1600/depositphotos_130962816-stock-illustration-cartoon-farmer-or-redneck.jpg"
+    t.text "avatar_url", default: "https://st3.depositphotos.com/6811030/13096/v/1600/depositphotos_130962816-stock-illustration-cartoon-farmer-or-redneck.jpg"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
