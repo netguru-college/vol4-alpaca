3c1f32ec4a410716d8ece704e6decf3991a0739a
katguz3485 <katarzyna.guzik01@gmail.com>
2019-02-19 11:38:47 +0100

    necessery before rebase

--- db/migrate/20190219095152_create_category_skills.rb
+++ db/migrate/20190219095152_create_category_skills.rb
@@ -1,6 +1,10 @@
 class CreateCategorySkills < ActiveRecord::Migration[5.2]
   def change
     create_table :category_skills do |t|
+      t.belongs_to :category, index: true, foreign_key: true
+      t.belongs_to :skill, index: true, foreign_key: true
+      t.integer :weight
+
 
       t.timestamps
     end
