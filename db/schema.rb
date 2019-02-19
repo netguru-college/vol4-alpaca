1c3b3c4185881bb4d04fb69d772c19580606b30a
c00lerxo <szymonkuleczka98@gmail.com>
2019-02-19 16:09:21 +0100

    Add picture_url to Category - migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_142429) do
+ActiveRecord::Schema.define(version: 2019_02_19_150250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_142429) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "picture_url"
   end
 
   create_table "category_skills", force: :cascade do |t|
