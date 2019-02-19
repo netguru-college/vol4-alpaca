88e141b4da0eca563ef2148bd1cd3bd93fd4f8ff
c00lerxo <szymonkuleczka98@gmail.com>
2019-02-19 15:31:51 +0100

    Add styling, _listing partial, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # put 'change_owner', to: "ownerships#change_owner", as: :change_
 
   resources :alpacas
+  resources :events
   # resources :ownerships
 
   resources :alpacas do
