d67774e10f0e471a57ca0ca63f59e2aa3488d8f5
c00lerxo <szymonkuleczka98@gmail.com>
2019-02-20 11:55:41 +0100

    Add basic methods in Event controller, add basic views and partials

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,23 +1,93 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @user = current_user
+    @events = Event.joins(:alpacas)
+                   .group('events.id')
+                   .having('count(alpaca_id) < 2')
   end
 
   def show
+    @event = Event.find(params[:id])
+    @alpacas = @event.alpacas
+    @user = current_user
   end
 
   def new
+    @event = Event.new
+    @alpacas = current_user.alpacas
   end
 
   def create
-  end
+    @event = Event.new(event_params)
+    @alpaca = current_user
+              .alpacas
+              .find(params[:event][:alpacas])
 
-  def edit
+    if @event.save
+      AlpacaEvent.new(
+                      alpaca_id: @alpaca.id,
+                      event_id: @event.id
+                     )
+                     .save
+      redirect_to @event
+    else
+      render 'new'
+    end
   end
 
   def update
+    @alpaca = Alpaca.find(params[:event][:alpacas])
+    @event = Event.find(params[:id])
+
+    if AlpacaEvent.new(
+                        alpaca_id: @alpaca.id,
+                        event_id: @event.id
+                      )
+                      .save
+
+      calculate_winner
+      redirect_to @event
+    else
+      render 'update'
+    end
   end
 
-  def delete
+  private
+
+  def calculate_winner
+    points = {}
+    p get_levels
+    get_levels.each do |alpaca_id, level|
+      points[alpaca_id] = @event
+                         .category
+                         .category_skills
+                         .first
+                         .weight * roll(1, 6) * level
+    end
+    points.max_by{ |alpaca_id, points| points }
+  end
+
+  def roll(amount = 0, sides = 0)
+    # rand(1..0) will return nil btw
+    amount.to_i.times.sum { |t| rand(1..sides.to_i) }
+  end
+
+  def get_levels
+    levels = {}
+    @event.alpacas.each do |alpaca|
+      @event.category.skills.each do |skill|
+        levels[alpaca.id] = AlpacaSkill.where(
+                                  alpaca_id: alpaca.id,
+                                  skill_id: skill.id
+                                )
+                                .first
+                                .level
+      end
+    end
+    return levels
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :category_id)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
             :description,
             presence: true
 
-  has_many :alpacas, through: :alpaca_events
   has_many :alpaca_events
+  has_many :alpacas, through: :alpaca_events
   belongs_to :category
 end
