db4dada7a35825f0e941a2ca62d02f080b49c7e9
voycik <voycik@gmail.com>
2019-02-20 14:00:18 +0100

    Seeds generate 5 logable users with 1 alpaca each and 20 alpacas on market

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,25 @@ Rails.logger.info 'Seeds crating started'
     password: "123qwe",
     name: Faker::Internet.username(5..8)
   )
+  user.save
+  alpaca = CreateRandomAlpaca.new.call
+  alpaca.save
+  ownership = Ownership.new
+  ownership.alpaca_id = alpaca.id
+  ownership.user_id = user.id
+  ownership.save
   @i += 1
-  user.save!
+
 end
 
-Rails.logger.info 'Seeds crating finished'
+Rails.logger.info '5 users created, where emails are user1@mail.com to user5@mail.com, password: 123qwe. Every user has one Alpaca.'
+
+20.times do
+
+  alpaca = CreateRandomAlpaca.new.call
+  alpaca.for_sale = true
+  alpaca.save
+
+end
+
+Rails.logger.info '20 Alpacas put on Market.'
