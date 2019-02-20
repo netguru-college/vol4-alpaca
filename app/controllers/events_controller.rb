5e9013ebbcc333c926a12fbfa801109bb0b529f4
c00lerxo <szymonkuleczka98@gmail.com>
2019-02-20 12:37:27 +0100

    Add calculate_event_winner service

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,7 +45,16 @@ class EventsController < ApplicationController
                       )
                       .save
 
-      calculate_winner
+      AlpacaEvent.where(
+                          event_id:   @event.id,
+                          alpaca_id:  CalculateEventWinner
+                                      .new(@event)
+                                      .call
+                        )
+                 #.update(
+                  #        winner:     true
+                  #      )
+
       redirect_to @event
     else
       render 'update'
@@ -53,39 +62,6 @@ class EventsController < ApplicationController
   end
 
   private
-
-  def calculate_winner
-    points = {}
-    p get_levels
-    get_levels.each do |alpaca_id, level|
-      points[alpaca_id] = @event
-                         .category
-                         .category_skills
-                         .first
-                         .weight * roll(1, 6) * level
-    end
-    points.max_by{ |alpaca_id, points| points }
-  end
-
-  def roll(amount = 0, sides = 0)
-    # rand(1..0) will return nil btw
-    amount.to_i.times.sum { |t| rand(1..sides.to_i) }
-  end
-
-  def get_levels
-    levels = {}
-    @event.alpacas.each do |alpaca|
-      @event.category.skills.each do |skill|
-        levels[alpaca.id] = AlpacaSkill.where(
-                                  alpaca_id: alpaca.id,
-                                  skill_id: skill.id
-                                )
-                                .first
-                                .level
-      end
-    end
-    return levels
-  end
 
   def event_params
     params.require(:event).permit(:name, :description, :category_id)
---
+++ app/services/calculate_event_winner.rb
@@ -0,0 +1,46 @@
+class CalculateEventWinner
+  def init(event)
+    @event = event
+  end
+
+  def call
+    calculate_winner.first
+  end
+
+  private
+
+  def calculate_winner
+    points = {}
+
+    get_levels.each do |alpaca_id, level|
+      points[alpaca_id] = @event
+                         .category
+                         .category_skills
+                         .first
+                         .weight * roll(1, 6) * level
+    end
+    points.max_by{ |alpaca_id, points| points }
+  end
+
+  def get_levels
+    levels = {}
+
+    @event.alpacas.each do |alpaca|
+      @event.category.skills.each do |skill|
+        levels[alpaca.id] = AlpacaSkill.where(
+                                              alpaca_id: alpaca.id,
+                                              skill_id: skill.id
+                                              )
+                                              .first
+                                              .level
+      end
+    end
+
+    return levels
+  end
+
+  def roll(amount = 0, sides = 0)
+    # rand(1..0) will return nil btw
+    amount.to_i.times.sum { |t| rand(1..sides.to_i) }
+  end
+end
