61e14ecfb5deb256986878d26630f0c97ac49cd7
voycik <voycik@gmail.com>
2019-03-20 21:33:50 +0100

    Add decorator spec

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator do
+  let(:user) { FactoryBot.build(:user, hay: 200)}
+  let(:decorator) {user.decorate}
+
+  context 'User has no name' do
+    it "should return User's fortune" do
+      expect(decorator.fortune).to eq ("User's fortune: 200")
+    end
+  end
+
+  context 'User has name' do
+    before do
+      user.name = 'John'
+    end
+    it "should return John's fortune" do
+      expect(decorator.fortune).to eq("John's fortune: 200")
+    end
+  end
 end
